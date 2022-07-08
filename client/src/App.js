a637538546867e6c32a5f3b9ae7d34d68729c538
Warrick Smith <warricksmith@developersinstitute.co.nz>
2022-07-09 09:51:04 +1200

    feta: add fetch bankdata

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [bankData, setBankData] = useState([]);
+
+  useEffect(() => {
+    // We use AbortController (https://developer.mozilla.org/en-US/docs/Web/API/AbortController)
+    // to clean up so that we don’t introduce a memory leak
+    // (https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup)
+    const abortController = new AbortController();
+
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        setError(false);
+        const result = await fetch("http://localhost:5000/api/transactions");
+        if (!result.ok) {
+          throw new Error("API Error");
+        }
+        const data = await result.json();
+        if (!abortController.signal.aborted) {
+          setBankData(data.banks);
+        }
+      } catch (error) {
+        if (!abortController.signal.aborted) {
+          setError(true);
+        }
+      } finally {
+        if (!abortController.signal.aborted) {
+          setLoading(false);
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => abortController.abort();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
