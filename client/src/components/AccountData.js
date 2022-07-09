0c9c80173de86693745799d3e23f77df32af98e7
Warrick Smith <warricksmith@developersinstitute.co.nz>
2022-07-09 14:45:51 +1200

    fix: remove divs

--- client/src/components/AccountData.js
+++ client/src/components/AccountData.js
@@ -4,32 +4,30 @@ const AccountData = ({ accountData }) => {
     <h2>No Account Data</h2>
   ) : (
     <>
-      <div className="tableclass">
-        <h3>Accounts Summary</h3>
-        <thead>
-          <tr>
-            <td>Account Name</td>
-            <td>Account Type</td>
-            <td>Available Balance</td>
-            <td>Current Balance</td>
-          </tr>
-        </thead>
-        <tbody>
-          {accountData.map(
-            (
-              { accountName, accountType, availableBalance, currentBalance },
-              index
-            ) => (
-              <tr key={index}>
-                <td>{accountName}</td>
-                <td>{accountType}</td>
-                <td>${availableBalance}</td>
-                <td>${currentBalance}</td>
-              </tr>
-            )
-          )}
-        </tbody>
-      </div>
+      <h3>Accounts Summary</h3>
+      <thead>
+        <tr>
+          <td>Account Name</td>
+          <td>Account Type</td>
+          <td>Available Balance</td>
+          <td>Current Balance</td>
+        </tr>
+      </thead>
+      <tbody>
+        {accountData.map(
+          (
+            { id, accountName, accountType, availableBalance, currentBalance },
+            index
+          ) => (
+            <tr key={index}>
+              <td>{accountName}</td>
+              <td>{accountType}</td>
+              <td>${availableBalance}</td>
+              <td>${currentBalance}</td>
+            </tr>
+          )
+        )}
+      </tbody>
     </>
   );
 };
