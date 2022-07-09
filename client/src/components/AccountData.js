fbf2d93b12e878bb88c52c5b6eab1bdfe51542c6
Warrick Smith <warricksmith@developersinstitute.co.nz>
2022-07-09 15:22:59 +1200

    fix: correct html table structure

--- client/src/components/AccountData.js
+++ client/src/components/AccountData.js
@@ -1,32 +1,40 @@
+import "./table.css";
 const AccountData = ({ accountData }) => {
   return !accountData ? (
-    <h2>No Account Data</h2>
+    <h3>No Account Data</h3>
   ) : (
     <>
       <h3>Accounts Summary</h3>
-      <thead>
-        <tr>
-          <td>Account Name</td>
-          <td>Account Type</td>
-          <td>Available Balance</td>
-          <td>Current Balance</td>
-        </tr>
-      </thead>
-      <tbody>
-        {accountData.map(
-          (
-            { id, accountName, accountType, availableBalance, currentBalance },
-            index
-          ) => (
-            <tr key={id}>
-              <td>{accountName}</td>
-              <td>{accountType}</td>
-              <td>${availableBalance}</td>
-              <td>${currentBalance}</td>
-            </tr>
-          )
-        )}
-      </tbody>
+      <table className="table">
+        <tbody>
+          <tr className="tablehead">
+            <td>Account Name</td>
+            <td>Account Type</td>
+            <td>Available </td>
+            <td>Current </td>
+          </tr>
+
+          {accountData.map(
+            (
+              {
+                id,
+                accountName,
+                accountType,
+                availableBalance,
+                currentBalance,
+              },
+              index
+            ) => (
+              <tr key={id} className="tablerow">
+                <td>{accountName}</td>
+                <td>{accountType}</td>
+                <td>${availableBalance}</td>
+                <td>${currentBalance}</td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
     </>
   );
 };
